1da1e8e22add1564f73df3f07393153e6665c9d4
zDerpz <114865054+zDerpz@users.noreply.github.com>
2024-10-21 11:01:43 +0100

    Create firebase.js

---
+++ firebase.js
@@ -0,0 +1,19 @@
+<script type="module">
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/10.14.1/firebase-app.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyCbGBgNrQaanRbNHJogV6OH4i_dPeU6VR0",
+    authDomain: "confused-7f79c.firebaseapp.com",
+    projectId: "confused-7f79c",
+    storageBucket: "confused-7f79c.appspot.com",
+    messagingSenderId: "78204722676",
+    appId: "1:78204722676:web:541b6085f3fc9736766da1"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+</script>
